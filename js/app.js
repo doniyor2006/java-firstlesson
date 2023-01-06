533dabe23e57180420594abda2e54d40958f0b4e
Doniyor <example@gmail.com>
2023-01-06 21:03:43 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let age = prompt('How much money do you have to travel abroad ?', 1500000);
+let transport_price = 500  //dollar 
+let hotel_price = 250  //dollar
+let sightseing_price = 120  //euro
+console.log(transport_price * 10.900 + hotel_price * 10.900 + sightseing_price * 12272.64);
+if(age >= 1480891.8){
+    alert ("Oq Yo'l,ALisher")
+}
+else{
+    alert ("Alisher,ozgina sabr qilish kerak bo'lar ekan")
+}
